dca4ff76aa77726472d0626305c6439fd127570a
herirudini <75158630+herirudini@users.noreply.github.com>
2021-06-09 12:24:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + 'dist/acongkelontong-fe'));
+app.use(express.static('dist/acongkelontong-fe'));
 app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, 'dist/acongkelontong-fe', 'index.html'))
+res.sendFile(path.join('dist/acongkelontong-fe', 'index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
