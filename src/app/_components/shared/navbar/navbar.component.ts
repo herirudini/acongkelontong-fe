f6585ab6c1daa91a754330c22a36190a7a2d80fc
herirudini <75158630+herirudini@users.noreply.github.com>
2021-06-12 17:23:02 +0700

    header

--- src/app/_components/shared/navbar/navbar.component.ts
+++ src/app/_components/shared/navbar/navbar.component.ts
@@ -7,11 +7,21 @@ import { AuthService } from 'src/app/_services/auth.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  role!: string | null;
+  rolelist!: any;
   constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.role = this.authService.getUserRole()
+    this.appendList()
   }
+  appendList() {
+    if (this.role === "owner") {
+      this.rolelist = ["owner", "inventory", "finance", "cashier"]
+    }
+
+  }
+
   logout() {
     this.authService.logout();
   }
