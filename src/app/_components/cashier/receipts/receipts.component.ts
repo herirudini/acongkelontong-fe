c7a1f4388623c321c2ed587799e945c3fffda2f4
dikiamust <agungmustaqimdiki@gmail.com>
2021-06-15 15:47:31 +0700

    list receipts added

--- src/app/_components/cashier/receipts/receipts.component.ts
+++ src/app/_components/cashier/receipts/receipts.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { CashierService } from 'src/app/_services/cashier.service';
 
 @Component({
   selector: 'app-receipts',
   templateUrl: './receipts.component.html',
-  styleUrls: ['./receipts.component.css']
+  styleUrls: ['./receipts.component.css'],
 })
 export class ReceiptsComponent implements OnInit {
+  response!: any[];
 
-  constructor() { }
+  constructor(private cashierService: CashierService) {}
 
   ngOnInit(): void {
+    this.cashierService.listReceipt().subscribe((response) => {
+      this.response = response;
+    });
   }
-
 }
