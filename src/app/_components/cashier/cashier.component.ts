1f97312c77e0efd34a6b52da3848f8af6aaafc48
herirudini <75158630+herirudini@users.noreply.github.com>
2021-06-15 19:29:01 +0700

    update

--- src/app/_components/cashier/cashier.component.ts
+++ src/app/_components/cashier/cashier.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
@@ -7,11 +8,9 @@ import { AuthService } from 'src/app/_services/auth.service';
   styleUrls: ['./cashier.component.css'],
 })
 export class CashierComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {}
-
-  logout() {
-    this.authService.logout();
+  ngOnInit(): void {
+    this.router.navigate(['/cashier/cart'])
   }
 }
